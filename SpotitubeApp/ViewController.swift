bf04c45e61cbee24ae35f925f8209cc9a597dc60
Vroth <keesbank666@gmail.com>
2015-11-27 15:21:49 +0100

    Initial Commit

---
+++ SpotitubeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotitubeApp
+//
+//  Created by apple on 27/11/15.
+//  Copyright © 2015 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
